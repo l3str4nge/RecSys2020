93142b5865d4d1b3ba0b13e672df1dc3e99894fe
ankonzoid <ankonzoid@users.noreply.github.com>
2020-06-14 19:51:28 -0400

    mvn opts

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ mkdir "${PROJECT_PATH}Models/XGB"
 
 mvn clean compile
 
-#need at least 300GB of RAM to run
+#need at least 30GB of RAM to run
 export MAVEN_OPTS="-Xmx250g -Xms250g"
 
 #parse all data
@@ -23,12 +23,14 @@ python get_tweet_text.py "${DATA_PATH}"
 mvn exec:java -Dexec.mainClass="recsys2020.RecSys20Text" -Dexec.args="${DATA_PATH}"
 
 #extract libsvm feature file for each engagement
+export MAVEN_OPTS="-Xmx230g -Xms230g"
 mvn exec:java -Dexec.mainClass="recsys2020.RecSys20Model" -Dexec.args="trainLIBSVM:ALL ${PROJECT_PATH}"
 
 #train XGB models
 mvn exec:java -Dexec.mainClass="recsys2020.RecSys20Model" -Dexec.args="trainXGB:ALL:true ${PROJECT_PATH}"
 
 #generate LB predictions, output will be in XGB_PATH
+export MAVEN_OPTS="-Xmx220g -Xms220g"
 mvn exec:java -Dexec.mainClass="recsys2020.RecSys20Model" -Dexec.args="submit:ALL ${PROJECT_PATH}"
 
 #generate TEST predictions, output will be in XGB_PATH
