f988ea6ed58c32e27ea3f915ea890038720554b9
ankonzoid <ankonzoid@users.noreply.github.com>
2020-06-15 01:59:48 -0400

    fix

--- run.sh
+++ run.sh
@@ -14,10 +14,10 @@ mvn clean compile
 export MAVEN_OPTS="-Xmx250g -Xms250g"
 
 #parse all data
-mvn exec:java -Dexec.mainClass="recsys2020.RecSys20DataParser" -Dexec.args="${DATA_PATH}"
+#mvn exec:java -Dexec.mainClass="recsys2020.RecSys20DataParser" -Dexec.args="${DATA_PATH}"
 
 #extract tweet text
-python3 src/main/python/tweets/get_tweet_text.py "${DATA_PATH}"
+python3 python/tweets/get_tweet_text.py "${DATA_PATH}"
 
 #parse tweet text
 mvn exec:java -Dexec.mainClass="recsys2020.RecSys20Text" -Dexec.args="${DATA_PATH}"
