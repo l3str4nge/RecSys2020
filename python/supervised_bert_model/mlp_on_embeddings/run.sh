19dce388aa0c28d2ec9af8b3b29369d06af764b6
Ravoxsg <mathieu.ravaut@student.ecp.fr>
2020-06-15 18:30:23 -0400

    run.sh

--- python/supervised_bert_model/mlp_on_embeddings/run.sh
+++ python/supervised_bert_model/mlp_on_embeddings/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 CUDA_VISIBLE_DEVICES=0,1 python3 main_feature_embedding_lb.py \
--d=../../../recsys_submission_data/training_inference_data/ \
+cd ..-d=../../../recsys_submission_data/training_inference_data/ \
 --emb_path=../../../recsys_submission_data/supervised_embeddings/bert/ \
 --checkpoint=../../../recsys_submission_data/model_weights/featurenet_supervised_difflr_new_split_32.ckpt \
 --emb_type=bert
