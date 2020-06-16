8976e920ebb8670b239213f974ad7e88d78e0e66
Hojin Yang <hojin.yang7@gmail.com>
2020-06-16 15:16:49 -0400

    Update run.sh

--- python/run.sh
+++ python/run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-mkdir ./data
-mkdir ./out
+PATH=`pwd`
 
 # Download the following files from S3:
 # 1. XLM-Roberta and Bert embeddings, placing them in /data/recsys2020/DL_Data/embeddings/xlm-r and /data/recsys2020/DL_Data//embeddings/bert respectively
@@ -10,7 +9,10 @@ mkdir ./out
 
 # Train the multilayer perceptron (MLP) models and then perform inference on the Valid, Submit and Test sets
 # The 4 csv output/submission files are saved under python/out/train(val)/modelname/engagements.csv
-mlp_model/run.sh
+
+cd $PATH"/mlp_model"
+run.sh
+cd $PATH
 
 # This concludes our "main pipeline". At this point we have 4/9 (+1 for the XGBoost model) of the models used in the final blend.
 # The following instructions will describe reproducing 1+(2) other models used in the blend: supervised fine-tuning of Bert
@@ -19,15 +21,18 @@ mlp_model/run.sh
 
 # Supervised Bert fine-tuning
 # Train the MLP model on supervised fine-tuned embeddings and then perform inference on the Valid, Submit and Test sets
-supervised_bert_model/mlp_on_embeddings/run.sh
+cd $PATH"/supervised_bert_model/mlp_on_embeddings"
+run.sh
+cd $PATH
 
 # The 4 csv output/submission files are saved under /data/recsys2020/DL_Ouputs/(train)(val)(submit)(test)/modelname/(engagement).csv
 
 
 # History embedding model
 # Train the MLP model on XLM-R embeddings with history and then perform inference on the Valid, Submit and Test sets
-history_model/run.sh
-
+cd $PATH"/history_model"
+run.sh
+cd $PATH
 # The 4 csv output/submission files are saved under /data/recsys2020/DL_Ouputs/(train)(val)(submit)(test)/modelname/(engagement).csv
 
 # Finally the prediction scores are blended
@@ -35,10 +40,11 @@ history_model/run.sh
 # (train)(val)(submit)(test)/modelname/(engagement).csv
 
 # run blender
-./post_processing/run_blender.sh
+cd $PATH"/post_processing"
+run_blender.sh
 # this should output the predictions from the blended model to /data/recsys2020/DL_Ouputs/test/blended
 
-
 # run temperature tuning (done only for 1 out of 2 submissions)
-./post_processing/run_temperature_scaling.sh
+run_temperature_scaling.sh
 # this should output the scaled predictions to /data/recsys2020/DL_Ouputs/test/blended_and_scaled
+cd $PATH
